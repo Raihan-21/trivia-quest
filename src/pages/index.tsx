58d84e709c236bdb9cf1f490df45e6edee46bc0d
Muhammad Raihan <muhammadraihan118@gmail.com>
2023-07-20 15:14:34 +0700

    add category step

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,69 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Input, Text } from "@chakra-ui/react";
 import { stayPixel } from "@/fonts/font";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
+import useFetch from "@/hooks/useFetch";
 
 export default function Home() {
+  const [steps, setSteps] = useState([
+    {
+      value: "step-1",
+
+      active: true,
+    },
+    {
+      value: "step-2",
+
+      active: false,
+    },
+    {
+      value: "step-3",
+
+      active: false,
+    },
+  ]);
   const [onClickedPlay, setOnClickedPlay] = useState(false);
   const [onPlay, setOnPlay] = useState(false);
-  useEffect(() => {}, [onPlay]);
+  const [withCategory, setWithCategory] = useState(false);
+  const [withoutCategory, setWithoutCategory] = useState(false);
+  const [tags, setTags] = useState<string[]>([]);
+  const [tagInput, setTagInput] = useState("");
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [fetchTags, tagsData, tagLoading] = useFetch(
+    "https://the-trivia-api.com/v2/tags"
+  );
+  const goToStep = useCallback(
+    (name: string) => {
+      setSteps(
+        steps.map((step) => {
+          if (step.value === name) {
+            return { ...step, active: true };
+          }
+          return { ...step, active: false };
+        })
+      );
+    },
+    [steps]
+  );
+  useEffect(() => {
+    fetchTags();
+    if (tagsData) setTags(tagsData.slice(0, 19));
+  }, [steps[2].active]);
+  useEffect(() => {
+    if (tagsData)
+      setTags(
+        tagsData.filter((tag: string) => tag.includes(tagInput)).slice(0, 19)
+      );
+  }, [tagInput]);
 
   return (
     <Box minHeight={"100vh"} width={"100vw"} backgroundColor={"purple.800"}>
       <AnimatePresence>
-        {!onPlay && (
+        {steps[0].active && (
           <Flex flexDirection={"column"} alignItems={"center"}>
             <motion.div
               exit={{ scale: 0, originX: 0.5, originY: "50%" }}
@@ -34,32 +83,195 @@ export default function Home() {
               exit={{ scale: 0, originX: 0.5, originY: "50%" }}
               transition={{ duration: 0.6, delay: 0.2 }}
             >
-              <Box
-                className={stayPixel.className}
-                backgroundColor={"white"}
-                width={"fit-content"}
-                paddingY={2}
-                paddingX={4}
-                borderRadius={10}
-                marginTop={50}
-                cursor={"pointer"}
-                boxShadow={"3px 3px 3px 3px black"}
-                transform={onClickedPlay ? "scale(0.8)" : "none"}
-                _hover={{ boxShadow: "none" }}
-                transition={"all 100ms ease-out"}
-                onClick={() => {
-                  setOnClickedPlay(true);
-                  setTimeout(() => {
-                    setOnClickedPlay(false);
-                  }, 100);
-                  setOnPlay(true);
-                }}
-              >
-                <Text fontSize={"2xl"}>Play now</Text>
-              </Box>
+              <motion.div whileTap={{ scale: 0.8 }}>
+                <Box
+                  className={stayPixel.className}
+                  backgroundColor={"white"}
+                  width={"fit-content"}
+                  paddingY={2}
+                  paddingX={4}
+                  borderRadius={10}
+                  marginTop={50}
+                  cursor={"pointer"}
+                  boxShadow={"3px 3px 3px 3px black"}
+                  _hover={{ boxShadow: "none" }}
+                  onClick={() => {
+                    setOnClickedPlay(true);
+                    setTimeout(() => {
+                      setOnClickedPlay(false);
+                    }, 100);
+                    setSteps(
+                      steps.map((step) => {
+                        if (step.value === "step-1")
+                          return { ...step, active: false };
+                        if (step.value === "step-2")
+                          return { ...step, active: true };
+                        return { ...step };
+                      })
+                    );
+                  }}
+                >
+                  <Text fontSize={"2xl"}>Play now</Text>
+                </Box>
+              </motion.div>
             </motion.div>
           </Flex>
         )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {steps[1].active && (
+          <Flex flexDirection={"column"} alignItems={"center"} paddingTop={40}>
+            <motion.div exit={{ scale: 0 }} transition={{ duration: 0.5 }}>
+              <motion.div
+                initial={{ scale: 0, originX: 0.5, originY: 0.5 }}
+                animate={{ scale: 1, originX: 0.5, originY: 0.5 }}
+                transition={{ duration: 0.5, delay: 0.9 }}
+              >
+                <Text color={"white"} fontSize={20}>
+                  Do you want to choose categories for your quest?
+                </Text>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              exit={{ scale: 0 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+            >
+              <Flex justifyContent={"center"} columnGap={5} marginTop={10}>
+                <motion.div
+                  initial={{ scale: 0, originX: 0.5, originY: 0.5 }}
+                  animate={{ scale: 1, originX: 0.5, originY: 0.5 }}
+                  transition={{ duration: 0.2, delay: 1.5 }}
+                >
+                  <motion.div whileTap={{ scale: 0.8 }}>
+                    <Box
+                      backgroundColor={"green.300"}
+                      color={"white"}
+                      paddingY={2}
+                      paddingX={10}
+                      borderRadius={5}
+                      fontSize={"large"}
+                      cursor={"pointer"}
+                      onClick={() => {
+                        setWithCategory(true);
+                        goToStep("step-3");
+                      }}
+                    >
+                      Yes
+                    </Box>
+                  </motion.div>
+                </motion.div>
+                <motion.div
+                  initial={{ scale: 0, originX: 0.5, originY: 0.5 }}
+                  animate={{ scale: 1, originX: 0.5, originY: 0.5 }}
+                  transition={{ duration: 0.2, delay: 1.8 }}
+                >
+                  <motion.div whileTap={{ scale: 0.8 }}>
+                    <Box
+                      backgroundColor={"red.500"}
+                      color={"white"}
+                      paddingY={2}
+                      paddingX={10}
+                      borderRadius={5}
+                      fontSize={"large"}
+                      cursor={"pointer"}
+                      onClick={() => {
+                        setWithoutCategory(false);
+                        goToStep("step-3");
+                      }}
+                    >
+                      No
+                    </Box>
+                  </motion.div>
+                </motion.div>
+              </Flex>
+            </motion.div>
+            {/* </Box> */}
+            {/* </motion.div> */}
+          </Flex>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {steps[2].active && (
+          <Flex flexDirection={"column"} alignItems={"center"} paddingTop={40}>
+            <motion.div
+              initial={{ scale: 0, originX: 0.5, originY: 0.5 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              <Text fontSize={20} color={"white"}>
+                Select your categories:
+              </Text>
+              <Input
+                backgroundColor={"white"}
+                marginBottom={5}
+                value={tagInput}
+                onChange={(e) => {
+                  setTagInput(e.target.value);
+                }}
+              />
+              <Flex columnGap={5}>
+                {selectedTags.length &&
+                  selectedTags.map((tag: string) => (
+                    <Box
+                      borderRadius={10}
+                      paddingY={1}
+                      paddingX={4}
+                      backgroundColor={"green.300"}
+                      cursor={"pointer"}
+                      onClick={() => {
+                        setSelectedTags(
+                          selectedTags.filter(
+                            (selectedTag) => selectedTag !== tag
+                          )
+                        );
+                      }}
+                    >
+                      {tag}
+                    </Box>
+                  ))}
+              </Flex>
+              <Flex
+                flexWrap={"wrap"}
+                maxWidth={"500px"}
+                columnGap={5}
+                rowGap={2}
+                marginTop={5}
+              >
+                {!tagLoading &&
+                  tags.map((tag: string) => (
+                    <Box
+                      borderRadius={10}
+                      paddingY={1}
+                      paddingX={4}
+                      backgroundColor={"white"}
+                      cursor={"pointer"}
+                      onClick={() => {
+                        setSelectedTags([...selectedTags, tag]);
+                      }}
+                    >
+                      {tag}
+                    </Box>
+                  ))}
+              </Flex>
+              {!tagInput && (
+                <Text fontSize={20} color={"white"}>
+                  ..and More
+                </Text>
+              )}
+            </motion.div>
+          </Flex>
+        )}
+        <Flex justifyContent={"center"}>
+          <Text
+            backgroundColor={"yellow.400"}
+            paddingY={2}
+            paddingX={4}
+            width={"fit-content"}
+            borderRadius={5}
+          >
+            Continue
+          </Text>
+        </Flex>
       </AnimatePresence>
     </Box>
   );
